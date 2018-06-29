d01e5eec9b7d89955c7297bb19445cb82be64e2a
liujixin <liujixin@pawpaw.com>
2018-06-29 17:24:32 +0800

    111

--- src/main/java/com/pawpaw/registry/BootStrap.java
+++ src/main/java/com/pawpaw/registry/BootStrap.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class Bootstrap {
+public class BootStrap {
 	public static void main(String[] args) {
-		 SpringApplication.run(Bootstrap.class, args);
+		SpringApplication.run(BootStrap.class, args);
 	}
 }
