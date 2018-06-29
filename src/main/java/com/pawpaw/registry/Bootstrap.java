7f8300061af614109b844d4499db1f9673152eec
liujixin <liujixin@pawpaw.com>
2018-06-29 13:55:31 +0800

    初始化

---
+++ src/main/java/com/pawpaw/registry/Bootstrap.java
@@ -0,0 +1,13 @@
+package com.pawpaw.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Bootstrap {
+	public static void main(String[] args) {
+		 SpringApplication.run(Bootstrap.class, args);
+	}
+}
