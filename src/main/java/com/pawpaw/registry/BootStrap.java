2b52190750b79f33a74250d1a02f26106d1b088c
Jixin Liu <187157249@qq.com>
2019-01-23 23:41:22 +0800

    fff

--- src/main/java/com/pawpaw/registry/BootStrap.java
+++ src/main/java/com/pawpaw/registry/BootStrap.java
@@ -1,13 +1,16 @@
 package com.pawpaw.registry;
 
+import com.pawpaw.framework.core.PawpawApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class BootStrap {
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(BootStrap.class).run(args);
-	}
+    public static void main(String[] args) {
+
+        PawpawApplication application = new PawpawApplication();
+        application.run(BootStrap.class, args);
+
+    }
 }
